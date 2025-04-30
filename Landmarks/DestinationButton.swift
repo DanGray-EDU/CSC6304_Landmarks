f8cb77b2cd0641b1461ff02130b42685e25cba7b
DanGray <grayda@merrimack.edu>
2025-04-29 23:03:05 -0400

    Added splash page to select the landmard the user is interested .

---
+++ Landmarks/DestinationButton.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationButton.swift
+//  Landmarks
+//
+//  Created by Dan Gray on 4/29/25.
+//
+
+import SwiftUI
+
+struct DestinationButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationButton()
+}
---
+++ Landmarks/LandmarkBG.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkBG.swift
+//  Landmarks
+//
+//  Created by Dan Gray on 4/29/25.
+//
+
+import SwiftUI
+
+struct LandmarkBG: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkBG()
+}
